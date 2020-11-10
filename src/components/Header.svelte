321a087a98cbdb00349c4bae7c6c9805919071dd
parhan <apriliandi642@gmail.com>
2020-11-11 04:24:28 +0700

    add header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,100 @@
+<script>
+   export let segment;
+</script>
+
+<style>
+   header {
+      display: flex;
+      padding-bottom: 20px;
+      flex-direction: column;
+      background-color: #212d38;
+      border-bottom: 5px solid #38444d;
+   }
+
+   div {
+      display: flex;
+      margin-bottom: 32px;
+      justify-content: center;
+   }
+
+   ul {
+      display: flex;
+      justify-content: space-around;
+   }
+
+   a {
+      color: #ffffff;
+      font-size: 1rem;
+      font-weight: bold;
+      letter-spacing: 1px;
+      text-decoration: none;
+      transition: color 0.1s;
+   }
+
+   span {
+      font-weight: bold;
+      margin-right: 1px;
+   }
+
+   a:hover,
+   .current__menu {
+      color: #4c70e7;
+      padding-bottom: 19px;
+      border-bottom: 5px solid #4c70e7;
+   }
+
+   @media screen and (min-width: 600px) {
+      a {
+         font-size: calc(1rem + 1px);
+      }
+   }
+
+   @media screen and (min-width: 800px) {
+      a:hover,
+      .current__menu {
+         padding-bottom: 20px;
+      }
+
+      a {
+         font-size: calc(1rem - 1px);
+      }
+   }
+
+   @media screen and (min-width: 1200px) {
+      a:hover,
+      .current__menu {
+         padding-bottom: 19px;
+      }
+   }
+</style>
+
+<header>
+   <div>
+      <img
+         src="https://www.gravatar.com/avatar/1605042201304?s=210&d=robohash"
+         loading="lazy"
+         alt="1605042201304" />
+   </div>
+
+   <ul>
+      <li>
+         <a
+            href="/"
+            class:current__menu={segment === undefined}><span>&#8725;</span>
+            home</a>
+      </li>
+      <li>
+         <a
+            href="/profile"
+            class:current__menu={segment === 'profile'}><span>&#8725;</span>
+            profile</a>
+      </li>
+      <li>
+         <a
+            href="/post"
+            class:current__menu={segment === 'post'}><span>&#8725;</span>
+            post</a>
+      </li>
+      <li><a href="/logout"><span>&#8725;</span> logout</a></li>
+   </ul>
+</header>
