f5beaf214f28237b194701c2cc87ff515ecf7f67
parhan <apriliandi642@gmail.com>
2020-11-14 01:36:18 +0700

    make logo component

---
+++ src/components/Logo.svelte
@@ -0,0 +1,18 @@
+<style>
+   .logo {
+      text-align: center;
+      margin-top: 50px;
+   }
+
+   .logo img {
+      width: 160px;
+      height: 160px;
+      color: #ffffff;
+      border-radius: 100%;
+      border: 5px solid #273742;
+   }
+</style>
+
+<div class="logo">
+   <img src="/images/my-logo.jpg" loading="lazy" alt="my_logo" />
+</div>
