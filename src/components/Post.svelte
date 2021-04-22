af80e1f5e0272dcbe2597b4d7ee09f910f46d1ce
Farhan Apriliandi <apriliandi642@gmail.com>
2021-04-22 15:25:21 +0700

    change like icon

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -45,12 +45,38 @@
    }
 </script>
 
-<!-- the post -->
 <div class="card cf" in:scale={{ duration: 400 }}>
    <div class="card__like">
-      <span class="card__like_icon" on:click={handleLike}>
-         {post.likes.includes($user.username) === true ? "💚" : "♡"}
-      </span>
+      <div class="card__like_icon" on:click={handleLike}>
+         {#if post.likes.includes($user.username) === true}
+            <svg
+               width="26px"
+               height="26px"
+               fill="#f01a1a"
+               viewBox="0 0 24 24"
+               xmlns="http://www.w3.org/2000/svg"
+            >
+               <path d="M0 0h24v24H0z" fill="none" />
+               <path
+                  d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+               />
+            </svg>
+         {:else}
+            <svg
+               width="26px"
+               height="26px"
+               fill="#ffffff"
+               viewBox="0 0 24 24"
+               xmlns="http://www.w3.org/2000/svg"
+            >
+               <path d="M0 0h24v24H0z" fill="none" />
+               <path
+                  d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"
+               />
+            </svg>
+         {/if}
+      </div>
+
       <span class="card__like_total"
          >{Intl.NumberFormat().format(post.likes.length)}</span
       >
@@ -76,6 +102,7 @@
       <p class="card__comments" on:click={showComments}>
          {showAllComments === true ? "hide comments" : "show comments"}
       </p>
+
       <p class="card__date">{new Time(post.date).fromNow("normal")}</p>
    </div>
 </div>
@@ -143,8 +170,9 @@
    }
 
    .card__like_total {
-      font-size: 1rem;
       color: #ffffff;
+      font-size: 1rem;
+      margin-top: -6px;
       margin-right: 5px;
       letter-spacing: 1.4px;
       font-family: monospace;
